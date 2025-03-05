661fed72728c4db86e7353f4de80eb0db25216ac
lateshaziczize <lateshaziczize@gmx.com>
2025-03-05 17:16:16 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    int result = num1 + num2;
+
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+
+    return 0;
+}
