b2940c883a904922e2ed420bf0f37f3a69add505
lateshaziczize <lateshaziczize@gmx.com>
2025-03-06 17:19:08 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,11 @@
+// Math Project 1
+
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+    printf("The sum of %d and %d is %d.\n", a, b, sum);
+    return 0;
+}
